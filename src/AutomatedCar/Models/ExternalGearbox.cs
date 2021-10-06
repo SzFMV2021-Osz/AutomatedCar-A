579175434cfa4a6af9187404739772df887eb751
DaKul28 <kulcsardaniel28@gmail.com>
2021-10-06 20:28:38 +0200

    Implemented external gearbox with unit test.

---
+++ src/AutomatedCar/Models/ExternalGearbox.cs
@@ -0,0 +1,52 @@
+﻿namespace AutomatedCar.Models
+{
+    using Avalonia.Media;
+    using ReactiveUI;
+    using System;
+    using SystemComponents;
+
+    public class ExternalGearbox : ReactiveObject
+    {
+        private int currentExternalGear = 0;  //0: P, 1: R, 2: N, 3: D
+        private AutomatedCar automatedCar;
+
+        public ExternalGearbox(AutomatedCar automatedCar)
+        {
+            this.automatedCar = automatedCar;
+        }
+
+        public int CurrentExternalGear
+        {
+            get
+            {
+                return this.currentExternalGear;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref this.currentExternalGear, value);
+            }
+        }
+
+        public void ExternalUpshift()
+        {
+            if (CurrentExternalGear != 3)
+            {
+                if ((automatedCar.Velocity.Y <= 0 && CurrentExternalGear == 2) || automatedCar.Speed == 0 || CurrentExternalGear == 1)
+                {
+                    CurrentExternalGear += 1;
+                }
+            }
+        }
+
+        public void ExternalDownshift()
+        {
+            if (CurrentExternalGear != 0)
+            {
+                if ((automatedCar.Velocity.Y >= 0 && CurrentExternalGear == 2) || automatedCar.Speed == 0 || CurrentExternalGear == 3)
+                {
+                    CurrentExternalGear -= 1;
+                }
+            }
+        }
+    }
+}
---
+++ test/AutomatedCarTests/ExternalGearboxUnitTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Tests
+{
+    using AutomatedCar.Models;
+    using Xunit;
+
+    public class ExternalGearboxUnitTest
+    {
+        private ExternalGearbox externalGearbox;
+        private AutomatedCar automatedCar;
+
+        public ExternalGearboxUnitTest()
+        {
+            automatedCar = new AutomatedCar(100, 100, "car_1_white.png");
+            externalGearbox = new ExternalGearbox(automatedCar);
+        }
+
+        [Fact]
+        public void ShouldExternalUpshift()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                externalGearbox.ExternalUpshift();
+            }
+
+            Assert.Equal(3, externalGearbox.CurrentExternalGear);
+        }
+
+        [Fact]
+        public void ShouldExternalDownShift()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                externalGearbox.ExternalUpshift();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                externalGearbox.ExternalDownshift();
+            }
+
+            Assert.Equal(1, externalGearbox.CurrentExternalGear);
+        }
+    }
+}
