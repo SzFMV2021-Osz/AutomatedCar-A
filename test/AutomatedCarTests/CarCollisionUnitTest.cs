7b2d711b0f5b725c9ee5da2e58b8c80c3867d1ef
Aron <ovariaron@gmail.com>
2021-10-07 19:14:21 +0200

    Unit tests done

--- test/AutomatedCarTests/CarCollisionUnitTest.cs
+++ test/AutomatedCarTests/CarCollisionUnitTest.cs
@@ -3,8 +3,14 @@
     using AutomatedCar;
     using AutomatedCar.Models;
     using AutomatedCar.SystemComponents;
+    using AutomatedCar.SystemComponents.Helpers;
+    using Avalonia;
     using Avalonia.Media;
+    using Newtonsoft.Json.Linq;
+    using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Reflection;
     using Xunit;
 
     public class CarCollisionUnitTest
@@ -20,11 +26,42 @@
         }
 
         [Fact]
-        public void GetsPolylines()
+        public void IndentifiesNpcCollisionType()
         {
-            PolylineGeometry pg = new PolylineGeometry();
+            Assert.Equal(CollisionType.NPC, collisionDetection.DetermineCollisionType(automatedCar));
+        }
 
-            Assert.Equal(pg.GetType(), collisionDetection.GetCarPoints(automatedCar).GetType());
+        [Fact]
+        public void IndentifiesStaticObjectCollisionType()
+        {
+            Assert.Equal(CollisionType.StaticObject, collisionDetection.DetermineCollisionType(new WorldObject(100, 100, "car_1_white.png")));
+        }
+
+        [Fact]
+        public void CarPointXCorrect()
+        {
+            automatedCar.Geometry = MockCarPoints();
+
+            // Expected 151, because car point X is 51 + word position 100
+            Assert.Equal(151, collisionDetection.GetCarPoints(automatedCar).Points[0].X);
+        }
+
+        [Fact]
+        public void CarPointYCorrect()
+        {
+            automatedCar.Geometry = MockCarPoints();
+
+            // Expected 339, because car point Y is 239 + word position 100
+            Assert.Equal(339, collisionDetection.GetCarPoints(automatedCar).Points[0].Y);
+        }
+
+        private PolylineGeometry MockCarPoints()
+        {
+            var points = new List<Point>();
+            
+            points.Add(new Point(51, 239));
+
+            return new PolylineGeometry(points, false);
         }
     }
 }
