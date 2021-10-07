24297022b4397f820c56db21370d1fdfbf3329f7
Aron <ovariaron@gmail.com>
2021-10-07 18:15:20 +0200

    tests started

---
+++ test/AutomatedCarTests/CarCollisionUnitTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests
+{
+    using AutomatedCar;
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents;
+    using Avalonia.Media;
+    using System.Linq;
+    using Xunit;
+
+    public class CarCollisionUnitTest
+    {
+
+        private AutomatedCar automatedCar;
+        private CollisionDetection collisionDetection;
+
+        public CarCollisionUnitTest()
+        {
+            automatedCar = new AutomatedCar(100, 100, "car_1_white.png");
+            collisionDetection = new CollisionDetection(automatedCar.VirtualFunctionBus);
+        }
+
+        [Fact]
+        public void GetsPolylines()
+        {
+            PolylineGeometry pg = new PolylineGeometry();
+
+            Assert.Equal(pg.GetType(), collisionDetection.GetCarPoints(automatedCar).GetType());
+        }
+    }
+}
