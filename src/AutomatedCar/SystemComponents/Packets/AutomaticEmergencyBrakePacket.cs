1da5a211a7c183bba9db01a70cd4606eaa0c469d
Kovács Olivér <oliver.kovacs313@gmail.com>
2021-10-18 14:49:54 +0200

    AEB packet implemented

---
+++ src/AutomatedCar/SystemComponents/Packets/AutomaticEmergencyBrakePacket.cs
@@ -0,0 +1,42 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+
+    public class AutomaticEmergencyBrakePacket : ReactiveObject, IAutomaticEmergencyBrakePacket
+    {
+        private bool needEmergencyBrakeWarning;
+        private bool mightNotWorkProperlyWarning;
+
+        /// <summary>
+        /// Gets or Sets the value indicating whether a visual warning is required to the driver when the collison is avoidable.
+        /// </summary>
+        public bool NeedEmergencyBrakeWarning 
+        {
+            get
+            {
+                return this.needEmergencyBrakeWarning;
+            }
+
+            set
+            {
+                this.RaiseAndSetIfChanged(ref this.needEmergencyBrakeWarning, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or Sets the value indicating whether the AEB cannot handle all situations above 70 km/h.
+        /// </summary>
+        public bool MightNotWorkProperlyWarning
+        {
+            get
+            {
+                return this.mightNotWorkProperlyWarning;
+            }
+
+            set
+            {
+                this.RaiseAndSetIfChanged(ref this.mightNotWorkProperlyWarning, value);
+            }
+        }
+    }
+}
