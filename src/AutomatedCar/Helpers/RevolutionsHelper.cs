2949f97e5e81d50383539fd617e8cd3d702eea53
DaKul28 <38877541+DaKul28@users.noreply.github.com>
2021-10-20 19:07:26 +0200

    Merge branch 'main' into #9-automatic-transmission

---
+++ src/AutomatedCar/Helpers/RevolutionsHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace AutomatedCar.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RevolutionsHelper
+    {
+        // Stores a value pair for each inner gear, with the second value representing the rate at which the RPM should increase in the respective gear
+        public static List<Tuple<int, double>> GearCoefficients { get; set; } = new List<Tuple<int, double>>()
+        {
+            new Tuple<int, double>(0, 1), // this is for N
+            new Tuple<int, double>(1, 2.5),
+            new Tuple<int, double>(2, 0.7),
+            new Tuple<int, double>(3, 0.55),
+            new Tuple<int, double>(4, 0.45),
+        };
+    }
+}
--- src/AutomatedCar/SystemComponents/Packets/ISensorPacket.cs
+++ src/AutomatedCar/SystemComponents/Packets/ISensorPacket.cs
@@ -5,10 +5,10 @@
 
     public interface ISensorPacket
     {
-        IWorldObject ClosestObject { get; set; }
+        IList<WorldObject> DetectedObjects { get; set; }
 
-        ICollection<IWorldObject> DetectedObjects { get; set; }
+        IList<WorldObject> RelevantObjects { get; set; }
 
-        ICollection<IWorldObject> RelevantObjects { get; set; }
+        WorldObject ClosestObject { get; set; }
     }
 }
