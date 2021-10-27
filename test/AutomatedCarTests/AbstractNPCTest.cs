ff87681a1cfe995394de9fd992f1b318d7e983bf
MadVisions <madbeno@gmail.com>
2021-10-27 21:08:12 +0200

    cleanup

--- test/AutomatedCarTests/AbstractNPCTest.cs
+++ test/AutomatedCarTests/AbstractNPCTest.cs
@@ -11,43 +11,6 @@
 
     public class AbstractNPCTest
     {
-        private class NPCPathDataProvider : IEnumerable<object[]>
-        {
-            IEnumerator<object[]> GetEnumerator()
-            {
-                yield return new object[]
-                {
-                    new List<Vector>()
-                    {
-                        new Vector(0, 0),
-                        new Vector(1, 1)
-                    },
-                    new Vector(5, 5)
-                };
-                yield return new object[]
-                {
-                    new List<Vector>()
-                    {
-                        new Vector(100, 100),
-                        new Vector(110, 110)
-                    },
-                    new Vector(5, 5)
-                };
-            }
-
-            IEnumerator<object[]> IEnumerable<object[]>.GetEnumerator() => GetEnumerator();
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        }
-
-        private AbstractNPC npc;
-        [Theory]
-        [ClassData(typeof(NPCPathDataProvider))]
-        public void GetDirectionTest(Vector[] path, Vector expectedDirection)
-        {
-            var yeppe = path;
-        }
-
         private class MovementTestDataProvider : IEnumerable<object[]>
         {
             IEnumerator<object[]> GetEnumerator()
